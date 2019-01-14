56a27eb2f1890324287e6c1b887239db10a797a4
super94black <931105779@qq.com>
2019-01-14 23:05:00 +0800

    直播间搭建完毕

---
+++ src/main/java/com/nanshanlive/dao/StatDao.java
@@ -0,0 +1,37 @@
+package com.nanshanlive.dao;
+
+import com.nanshanlive.entity.Guest;
+import com.nanshanlive.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+@Repository
+public class StatDao {
+    @Autowired
+    RedisTemplate redisTemplate;
+    public void pushOnlineUser(UserEntity userEntity){
+        redisTemplate.opsForSet().add("OnlineUser",userEntity);
+    }
+    public void popOnlineUser(UserEntity userEntity){
+        redisTemplate.opsForSet().remove("OnlineUser" ,userEntity);
+    }
+    public Set getAllUserOnline(){
+        return redisTemplate.opsForSet().members("OnlineUser");
+    }
+    public void pushGuestHistory(Guest guest){
+        //最多存储指定个数的访客
+        if (redisTemplate.opsForList().size("Guest") == 2000l){
+            redisTemplate.opsForList().rightPop("Guest");
+        }
+        redisTemplate.opsForList().leftPush("Guest",guest);
+    }
+    public List getGuestHistory(){
+        return redisTemplate.opsForList().range("Guest",0,-1);
+    }
+}
---
+++ src/main/java/com/nanshanlive/service/AppListener.java
@@ -0,0 +1,16 @@
+package com.nanshanlive.service;
+
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.web.socket.messaging.SessionConnectedEvent;
+
+/**
+ * Created by Administrator on 2017/6/14.
+ */
+public class AppListener implements ApplicationListener<SessionConnectedEvent> {
+    @Override
+    public void onApplicationEvent(SessionConnectedEvent sessionConnectedEvent) {
+        StompHeaderAccessor sha = StompHeaderAccessor.wrap(sessionConnectedEvent.getMessage());
+        System.out.println(sha.getDestination());
+    }
+}
